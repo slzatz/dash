3cd154d0870ae615c0f6b71f400927fc332b8241
slzatz <slzatz@gmail.com>
2018-03-13 11:50:52 -0400

    added dash_grid_mqtt3.py: does callbacks programatically

---
+++ dash_grid_mqtt3.py
@@ -0,0 +1,179 @@
+'''
+phrases are a list of tuples like [('{}', 'forecast: '), ('{red}', '113.8M')]
+
+*0=temp sensors (CT, NYC): 4 wide
+*1=news feeds (WSJ, NYT, ArsTechnica, Reddit All, Twitter): 8 wide
+*2=stock quote: 4 wide
+*3=ToDos: 8 wide
+*4=google: 8 wide plus ?sonos status (PLAYING, TRANSITIONING, STOPPED) broadcast by sonos_track_info on topic esp_tft and also on sonos/{loc}/status for esp_tft_mqtt_photos(and lyrics) 
+*5=sales forecast: 4 wide
+*6=outlook_schedule: 8 wide
+7=artist image
+8=lyrics
+9=track_info broadcast by sonos_track_info.py
+10=sonos status (PLAYING, TRANSITIONING, STOPPED
+*11=sales top opportunities: 8 wide and need to change font
+12=Reminders (alarms) 
+13=Ticklers
+14=Facts
+*15=weather/tides: 4 wide
+*16=Industry: 8 wide
+17=temp sensor to do: if payload hasn't changed no need to write to screen
+'''
+
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import datetime
+import json
+import re
+import plotly
+from config import aws_mqtt_uri
+from flask_mqtt import Mqtt
+
+TEMP1=0
+NEWS=1 #news feeds(WSJ, NYT, ArsTechnica, Reddit All, Twitter)
+STOCK_QUOTE=2 #stock quote from whatever symbol chosen
+TODOS=3 #todos taken from listmanager starred items from work
+GOOGLE=4 #gmail alternating with google calendar
+SALES_FORECAST=5 #esp_tft_mqtt_sf2.py
+OUTLOOK=6 
+ARTIST_IMAGE=7
+LYRICS=8
+TRACK_INFO=9
+SONOS_STATUS=10
+TOP_SALES=11
+REMINDERS=12
+TICKLERS=13
+FACTS=14
+WEATHER=15 #also does tides
+INDUSTRY=16
+TEMP2=17 #another temp sensor slot
+
+LAYOUT =   [[(OUTLOOK, GOOGLE), ("six", "six")],
+           [(TODOS, NEWS), ("six", "six")],
+           [(SALES_FORECAST, WEATHER), ('six', 'six')],
+           [(TOP_SALES, INDUSTRY), ("two", "ten")],
+           [(TEMP1, STOCK_QUOTE), ("six", "six")],
+           ]
+
+app = dash.Dash(__name__)
+
+app.config['MQTT_BROKER_URL'] = aws_mqtt_uri 
+app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+app.config['MQTT_KEEPALIVE'] = 60  # set the time interval for sending a ping to the broker to 60 seconds
+app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+
+mqtt = Mqtt(app)
+
+data={} #data coming back from mqtt
+
+# line => "forecast: {red} 114.2M {}"
+#phrases = [('{}', 'forecast: '), ('{red}', '114.2M')]
+def get_phrases(line, start='{}'):
+    if line.find('{') == -1:
+        print("phrases =", [(start, line)])
+        return [(start, line)]
+
+    if line[0]!='{':
+        line = start+line
+
+    line = line+'{}'
+
+    z = re.finditer(r'{(.*?)}', line)
+    s = [[m.group(), m.span()] for m in z]
+    #print(s)
+    if not s:
+        return [('{}', line)]
+    phrases = []
+    for j in range(len(s)-1):
+        phrases.append((s[j][0],line[s[j][1][1]:s[j+1][1][0]]))
+    print("phrases =", phrases)
+    return phrases
+
+def generate_html(n, backgroundcolor='yellow'):
+    data_n = data.get(n)
+    if not data_n:
+        return [html.H3("No data")]
+
+    text = data_n.get('text', "Somehow no text key")
+    header = data_n.get('header', "Somehow no header key")
+    new_text = [html.H3(header)]
+    for line in text:
+        phrases = get_phrases(line)
+        new_line=[]
+        for phrase in phrases:
+            color = phrase[0][1:-1] 
+            new_line.append(html.Span(phrase[1], style={'color':color}) if color else phrase[1])
+        new_text.extend(new_line)
+        new_text.append(html.Br())
+
+    span_style = {'fontSize': '16px'}
+
+    div_style = {'fontSize': '16px',
+                 'backgroundColor':backgroundcolor,
+                 'borderWidth':'medium',
+                 'borderColor':'black',
+                 'borderStyle':'solid'}
+
+    return [html.Div([html.Span(new_text, style=span_style)], style=div_style)]
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('esp_tft')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    global data
+    payload = json.loads(message.payload.decode())
+    data[payload['pos']] = payload   
+    
+def create_layout():
+
+    layout = []
+    for item in LAYOUT:
+        id0 = 'live-update-text-'+str(item[0][0])
+        id1 = 'live-update-text-'+str(item[0][1])
+        class_name0 = "{} columns".format(item[1][0])
+        class_name1 = "{} columns".format(item[1][1])
+
+        layout.append(
+                     html.Div(
+                             [
+                                html.Div([
+                                    html.Div(id=id0),
+                                ], className=class_name0),
+
+                                html.Div([
+                                    html.Div(id=id1),
+                                ], className=class_name1)
+                                                     ],
+                            className="row")
+                     )
+    return layout
+
+layout = [
+    dcc.Interval(
+        id='interval-component',
+        interval=5000, # in milliseconds
+        n_intervals=0)
+         ]
+
+layout.extend(create_layout())
+app.layout = html.Div(layout)
+
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+def create_callback(n):
+    def callback(input_value): # I think this is n_intervals
+        return generate_html(n)
+    return callback
+
+for row in LAYOUT:
+    for item_num in row[0]:
+        f = create_callback(item_num)
+        app.callback(Output(f'live-update-text-{item_num}', 'children'), [Input('interval-component', 'n_intervals')])(f)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
